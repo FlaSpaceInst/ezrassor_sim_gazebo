9375678af45b93efc98bc534e64bcdb44d69705c
CSharpRon <ronald.marrero@outlook.com>
2021-01-10 18:28:06 -0500

    printing env

--- test/check-environment.sh
+++ test/check-environment.sh
@@ -9,13 +9,13 @@ pkg_resources="install/ezrassor_sim_gazebo/share/ezrassor_sim_gazebo/worlds"
 if echo "$models_path" | grep -q "$pkg_models"; then
     echo "GAZEBO_MODEL_PATH set correctly"
 else
-    echo "$pkg_models not found in GAZEBO_MODEL_PATH"
+    echo "$pkg_models not found in GAZEBO_MODEL_PATH: $models_path"
     return 1
 fi
 
 if echo "$resources_path" | grep -q "$pkg_resources"; then
     echo "GAZEBO_RESOURCE_PATH set correctly"
 else
-    echo "$pkg_resources not found in GAZEBO_RESOURCE_PATH"
+    echo "$pkg_resources not found in GAZEBO_RESOURCE_PATH: $resources_path
     return 1
 fi
