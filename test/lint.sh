e9d1b91d07e4ea3f656967ce67c5bac9d2e0bf83
CSharpRon <ronald.marrero@outlook.com>
2021-01-11 09:41:23 -0500

    Fixing workflow again

--- test/lint.sh
+++ test/lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # flake8: noqa
 
-python3 -m pip install --upgrade pip black flake8
-python3 -m black --check .
+python3 -m pip install --upgrade pip black flake8 && \
+python3 -m black --check . && \
 python3 -m flake8 .
